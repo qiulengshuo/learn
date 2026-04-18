dfd41c725c0b126ae0f72b4a1a5b07a6eedd6d43
qiulengshuo <qiulengshuo@sina.com>
2026-04-18 15:25:18 +0800

    Revert "chore: initial project setup"
    This reverts commit 537920ad96a4fd675930e614fa5bde357d7f3782.

--- utils/price.js
+++
@@ -1,6 +0,0 @@
-// utils/price.js
-export function calculateTotal(items, discountRate = 0) {
-    if (!items || items.length === 0) return 0;
-    const subtotal = items.reduce((sum, item) => sum + item.price * item.quantity, 0);
-    return subtotal * (1 - discountRate);
-}
